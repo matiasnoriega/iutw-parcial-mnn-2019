7afa02a04690c2e01725eb11b1c55f8f126f2d14
matiasnoriega <matiasnoriega86@gmail.com>
2019-06-18 22:35:02 -0300

    crear usuarios service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { UsuariosComponent } from './usuarios/usuarios.component';
+import { UsuariosService } from './usuarios.service';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
   declarations: [ AppComponent, UsuariosComponent ],
-  bootstrap:    [ AppComponent ]
+  bootstrap:    [ AppComponent ],
+  providers: [UsuariosService]
 })
 export class AppModule { }
