2b030bf0bc27f8a5f133b475a2c286d0e752c5b1
matiasnoriega <matiasnoriega86@gmail.com>
2019-06-18 22:16:42 -0300

    eliminar hello component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
 import { UsuariosComponent } from './usuarios/usuarios.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, UsuariosComponent ],
+  declarations: [ AppComponent, UsuariosComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
