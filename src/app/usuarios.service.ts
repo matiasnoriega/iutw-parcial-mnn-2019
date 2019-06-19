dee7a8bb067862609950ea357d7464bb0bcd2275
matiasnoriega <matiasnoriega86@gmail.com>
2019-06-18 22:44:22 -0300

    UsuariosService::getUsuarios()

--- src/app/usuarios.service.ts
+++ src/app/usuarios.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { Usuario } from './usuario';
+import { HttpClient, HttpHeaders } from '@angular/common/http'
 
 @Injectable(
   {
@@ -8,6 +11,14 @@ import { Injectable } from '@angular/core';
 
 export class UsuariosService {
 
-  constructor() { }
+  private url = 'reqres.in/api/users?per_page=10';
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  getUsuarios(): Observable<Usuario[]>{
+      return this.http.get<Usuario[]>(this.url);
+  }
 
 }
