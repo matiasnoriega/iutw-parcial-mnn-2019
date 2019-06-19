78918073be6937f12d6c332f98193b2a6ec26b14
matiasnoriega <matiasnoriega86@gmail.com>
2019-06-18 22:51:23 -0300

    fix import HttpClientModule en app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { UsuariosComponent } from './usuarios/usuarios.component';
 import { UsuariosService } from './usuarios.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ BrowserModule, FormsModule, HttpClientModule ],
   declarations: [ AppComponent, UsuariosComponent ],
   bootstrap:    [ AppComponent ],
   providers: [UsuariosService]
